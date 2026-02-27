6368ab0b3bdb311f0aa8edcc1bf64fe81333c5c6
juan <juanmarulandadelosri@cmail.carleton.ca>
2026-02-27 03:19:17 -0500

    added printing to my kernel

--- run.sh
+++ run.sh
@@ -10,9 +10,10 @@ CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fuse-ld=lld
 
 # Build the kernel
 $CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
-    kernel.c
+    kernel.c common.c
 
 # start qemu
-$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
+    -kernel kernel.elf
 
 
