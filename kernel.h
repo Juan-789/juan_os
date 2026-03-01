5ccba59f06dcd6357944551a97e2b0baac801d0e
juan <juanmarulandadelosri@cmail.carleton.ca>
2026-03-01 12:30:23 -0500

    implemented context switching

--- kernel.h
+++ kernel.h
@@ -1,5 +1,10 @@
 #include "common.h"
 
+#define PROCS_MAX 8       // Maximum number of processes
+
+#define PROC_UNUSED   0   // Unused process control structure
+#define PROC_RUNNABLE 1   // Runnable process
+
 struct trap_frame {
     uint32_t ra;
     uint32_t gp;
@@ -65,3 +70,13 @@ struct sbiret {
         printf("PANIC: %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__);  \
         while (1) {}                                                           \
     } while (0)
+
+
+
+
+struct process {
+    int pid;             // Process ID
+    int state;           // Process state: PROC_UNUSED or PROC_RUNNABLE
+    vaddr_t sp;          // Stack pointer
+    uint8_t stack[8192]; // Kernel stack
+};
