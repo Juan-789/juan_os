75b8ce81fb2caf40b12fe303788e325828960fd7
juan <juanmarulandadelosri@cmail.carleton.ca>
2026-03-19 15:22:15 -0400

    implemented initial system calls

--- shell.c
+++ shell.c
@@ -1,11 +1,11 @@
-// #include "user.h"
+#include "kernel.h"
 
 
 // extern char _binary_shell_bin_start[] = "<shell.bin contents here>";
 // extern char _binary_shell_bin_size[];
 
 void main(void) {
-    *((volatile int *) 0x80200000) = 0x1234;
-    for(;;);
-
+    // *((volatile int *) 0x80200000) = 0x1234;
+    // for(;;);
+    printf("Hello world from shell\n");
 }
